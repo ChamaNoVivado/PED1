1939bc06ccc063817f38adbbaab8cea24482298c
Peedrooo <pedrovitora.jesus@gmail.com>
2021-10-21 13:37:29 -0300

    Final_Version Display_Basys3 :blush:

--- Semana 14/Trabalho_7_Grupo_12/Trabalho_7_Grupo_12.runs/impl_1/rundef.js
+++ Semana 14/Trabalho_7_Grupo_12/Trabalho_7_Grupo_12.runs/impl_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "write_bitstream", "begin" );
+ISETouchFile( "init_design", "begin" );
 ISEStep( "vivado",
          "-log main_counter_async_0toF.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source main_counter_async_0toF.tcl -notrace" );
 
